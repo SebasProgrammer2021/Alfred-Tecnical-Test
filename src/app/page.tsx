28b87cfe35d98078f6200cb159544f300e8c76da
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-03-20 07:58:00 -0500

    Actualiza las dependencias en package.json y package-lock.json, añadiendo zustand y bibliotecas de testing. Se implementa un componente Counter en page.tsx que utiliza el estado de zustand.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+"use client"
+import useStore from "@/store/store";
 import Image from "next/image";
+
+function Counter() {
+  const { count, inc } = useStore()
+  return (
+    <div>
+      <span className="border border-amber-200 p-4 block">{count}</span>
+      <br/>
+      <button onClick={inc}>one up</button>
+    </div>
+  )
+}
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>test</h1>
+        <Counter />
       </main>
     </div>
   );
