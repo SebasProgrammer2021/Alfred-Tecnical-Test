ac0b0a68ddab13f30ed2917c2006252573a9ed15
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-03-21 08:06:32 -0500

    Refactoriza el layout y la página principal. Se reemplaza la fuente Geist por Gotham Black en layout.tsx y se ajusta la opacidad del fondo en page.tsx. Se añade un archivo de tipos globales para manejar extensiones de archivos como CSS, SVG, PNG, entre otros.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,21 +29,18 @@ export default function Home() {
   }
 
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen sm:p-6 md:p-8 bg-[url(/bg-home.svg)] bg-cover bg-opacity-50 before:content-[''] before:absolute before:inset-0 before:bg-black before:opacity-40 before:z-0">
+    <div className="relative flex flex-col items-center justify-center min-h-screen sm:p-6 md:p-8 bg-[url(/bg-home.svg)] bg-cover bg-opacity-50 before:content-[''] before:absolute before:inset-0 before:bg-black before:opacity-20 before:z-0">
       <div className="relative z-10 flex flex-col items-center justify-center w-full">
         <h1 className="text-[58.91px] lg:text-[88.91px] font-black text-center mb-[141px] bg-gradient-to-r from-[#006AFF] to-[#00F9FF] text-transparent bg-clip-text font-gotham">SkyConnect Explorer</h1>
-        {/* <main className="flex-grow">
-          <AirportTable />
-        </main> */}
         <div className="relative w-full px-10">
           <form onSubmit={handleSearch} className="flex flex-col gap-[30.46px] items-center">
             <input
               type="text"
-              placeholder="Buscar aeropuertos"
+              placeholder="Buscar aeropuertos..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full px-4 py-2 border rounded-full bg-white
-            text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full xl:w-[780px] px-4 py-4 rounded-full bg-white
+            text-[#006FEE] placeholder:text-[#006FEE] placeholder:font-normal placeholder:text-xl/[36.59px] focus:outline-none"
             />
             <button
               className="bg-gradient-to-r from-[#0060FF] to-[#00FFE7] w-fit py-[7.8px] px-[31.2px] pr-22 border-white border-[1.3px] rounded-[10.4px] font-medium text-[19.5px]/[36.4px] line flex items-center gap-[15.6px]"
---
+++ src/types/global.d.ts
@@ -0,0 +1,45 @@
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.less' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+// También podemos agregar otras extensiones de archivos comunes en proyectos web
+declare module '*.svg' {
+  const content: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+} 
